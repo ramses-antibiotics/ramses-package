ea0588330967dad29f15f3166547fe1554845106
Peter Dutey <p.dutey-magni@ucl.ac.uk>
2022-08-02 12:14:21 +0100

    fix bug #84

--- tests/testthat/test-metadata.R
+++ tests/testthat/test-metadata.R
@@ -6,6 +6,13 @@ test_that("ICD-10-CM downloads and maps to Charlson", {
   expect_true(icd10_charlson[icd10_charlson$icd_code=="J440", "comorb"] == "copd")
   expect_true(icd10_charlson[icd10_charlson$icd_code=="J440", "comorb_group"] == "copd")
   expect_equal(icd10_charlson[icd10_charlson$icd_code=="J440", "charlson_weights"], 1)
+  
+  # Bug 84 fix verification
+  icd10cm_tbl <- dplyr::as_tibble(icd10cm)
+  icd10_charlson_tbl <- map_charlson_comorbidities(df = icd10cm_tbl, icd_column = "icd_code")
+  expect_true(icd10_charlson_tbl[icd10_charlson_tbl$icd_code=="J440", "comorb"] == "copd")
+  expect_true(icd10_charlson_tbl[icd10_charlson_tbl$icd_code=="J440", "comorb_group"] == "copd")
+  expect_equal(icd10_charlson_tbl[icd10_charlson_tbl$icd_code=="J440", "charlson_weights"], 1)
 })
 
 test_that("Infection mapping", {
